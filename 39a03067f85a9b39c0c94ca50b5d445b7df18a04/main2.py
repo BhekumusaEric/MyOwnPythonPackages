041434bd10c1c54597aa29150ec87fb12c7d531d
BhekumusaEric <kelvinbacela@gmail.com>
2024-06-20 00:25:32 +0200

    Demonstration of module creation and packaging as well as path appending

---
+++ 39a03067f85a9b39c0c94ca50b5d445b7df18a04/main2.py
@@ -0,0 +1,46 @@
+from sys import path
+path.append('..∖∖packages')
+
+import extra.iota
+print(extra.iota.FunI())
+
+from sys import path
+
+path.append('..∖∖packages')
+
+import extra.good.best.sigma
+from extra.good.best.tau import FunT
+
+print(extra.good.best.sigma.FunS())
+print(FunT())
+
+from sys import path
+
+path.append('..∖∖packages')
+
+
+import extra.good.best.sigma as sig
+import extra.good.alpha as alp
+
+print(sig.FunS())
+print(alp.FunA())
+
+from sys import path
+
+path.append('..∖∖packages∖∖extrapack.zip')
+
+import extra.good.best.sigma as sig
+import extra.good.alpha as alp
+from extra.iota import funI
+from extra.good.beta import funB
+
+print(sig.FunS())
+print(alp.FunA())
+print(funI())
+print(funB())
+
+
+
+
+
+
